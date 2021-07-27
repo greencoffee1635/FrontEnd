e3a5b324f74615b5ae353a1e6a9068bdeccb0ca4
choice <wer1264@gmail.com>
2021-07-27 20:59:58 +0900

    feat: configureStore 기초작업

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+import { store } from "./redux/confitureStore";
 
 import App from "./shared/App";
 
@@ -7,10 +10,10 @@ import GlobalStyle from "./shared/GlobalStyle";
 
 ReactDOM.render(
   // <React.StrictMode>
-  <>
+  <Provider store={store}>
     <App />
     <GlobalStyle />
-  </>,
+  </Provider>,
   // </React.StrictMode>,
   document.getElementById("root")
 );
