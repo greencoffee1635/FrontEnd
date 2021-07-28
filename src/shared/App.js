10638a2e65b94057345a0e8d831b07dd7d85afe6
choice <wer1264@gmail.com>
2021-07-28 21:11:06 +0900

    feat: 메인화면에서 시작버튼 눌렀을 때 화면이동 연결

--- src/shared/App.js
+++ src/shared/App.js
@@ -3,6 +3,7 @@ import { Route, BrowserRouter } from "react-router-dom";
 
 // pages
 import Home from "../pages/Home";
+import Option01 from "../pages/Option01";
 import Detail from "../pages/Detail";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
       <BrowserRouter>
         <Route exact path="/" component={Home} />
+        <Route exact path="/option01" component={Option01} />
         <Route exact path="/detail" component={Detail} />
         {/* 옵션화면 Route */}
       </BrowserRouter>
