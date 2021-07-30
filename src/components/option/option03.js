807a96a10fb67d54e8d38fa05316e3a1915d50f7
KWAK-AREUM <kal8518@naver.com>
2021-07-30 12:09:00 +0900

    feat: css 변경
    scss를 styled-components로 변경

--- src/components/option/option03.js
+++ src/components/option/option03.js
@@ -1,41 +1,92 @@
 import React from "react";
+import styled from "styled-components";
 
 // shared
 import Header from "../../shared/Header";
-import Layout from "../../shared/Layout";
 
 // components
 
 // css
-import "../../css/option/text.scss";
-import "../../css/option/button.scss";
 
-const option03 = (props) => {
+function Option03 (props) {
   return (
     <>
       <Header {...props} />
-      <Layout>
-        <div>
-          <text className="number">3/10</text>
-          <br />
-          <text className="question">언제 출발 예정이신가요?</text>
-        </div>
+      <Container>
+        <Box>
+          <Number>3/10</Number>
+          <Question>언제 출발 예정이신가요?</Question>
+        </Box>
 
-        <div className="center">
-          <div className="calendar">달력</div>
 
-          <button
-            className="nextbutton"
-            onClick={() => {
-              props.history.push("/option04");
-            }}
-          >
-            다음으로
-          </button>
-        </div>
-      </Layout>
+        <Calendar>달력</Calendar>
+
+        <NextButton
+          onClick={() => {props.history.push("/Option03");}}
+        >
+          다음으로
+        </NextButton>
+
+      </Container>
     </>
   );
 };
 
-export default option03;
+const Container = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+  width: 30vw;
+  height: 75%;
+  border: 1px solid black;
+  // display: flex;
+  // flex-direction: column;
+  // justify-content: center;
+  // align-items: center;
+
+`;
+
+const Box = styled.div`
+`;
+
+const Number = styled.p`
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #BBBBBB;
+  margin: 10px auto;
+`;
+
+const Question = styled.p`
+  font-weight: bolder;
+  font-size: 2.5rem;
+  width: 22vw;
+  height: 8vh;
+`;
+
+const ButtonBox = styled.div`
+
+`;
+
+const NextButton = styled.button`
+  margin: 50px auto ;
+  width: 22vw;
+  height: 7.5vh;
+  border: none;
+  border-radius: 125px;
+  background-color: #484848;
+  font-size: 2rem;
+  color: #fff;
+  cursor: pointer;
+`;
+
+const Calendar = styled.div`
+  width: 20vw;
+  height: 45vh;
+  border: 2px solid #484848;
+  margin: 15px auto;
+  font-size: 1.5rem;
+`;
+
+export default Option03;
+
--- src/shared/App.js
+++ src/shared/App.js
@@ -3,16 +3,8 @@ import { Route, BrowserRouter } from "react-router-dom";
 
 // pages
 import Home from "../pages/Home";
-import {
-  Option01,
-  Option02,
-  Option03,
-  Option04,
-  Option05,
-  Option06_0,
-  Option06_1,
-} from "../pages/Option";
-import option01 from "../components/option/option01";
+
+import Option01 from "../components/option/Option01";
 import option02 from "../components/option/option02";
 import option03 from "../components/option/option03";
 import option04 from "../components/option/option04";
@@ -26,7 +18,7 @@ function App() {
     <>
       <BrowserRouter>
         <Route exact path="/" component={Home} />
-        <Route exact path="/option01" component={option01} />
+        <Route exact path="/option01" component={Option01} />
         <Route exact path="/option02" component={option02} />
         <Route exact path="/option03" component={option03} />
         <Route exact path="/option04" component={option04} />
