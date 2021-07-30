91674911476ae36e9ddb177cecf3503d65964fa0
choice <wer1264@gmail.com>
2021-07-30 10:30:18 +0900

    Styled: Layout, Background scss에서 styled-components로 변경

--- src/components/home/Background.js
+++ src/components/home/Background.js
@@ -7,10 +7,21 @@ import bgImg from "../../images/background_img.jpg";
 function Background(props) {
   return (
     <>
+      <BackgroundColor />
       <BackgroundImg />
     </>
   );
 }
+
+const BackgroundColor = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 2;
+  background-color: rgba(0, 0, 0, 0.3);
+`;
 
 const BackgroundImg = styled.div`
   position: fixed;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,13 +9,12 @@ import Background from "../components/home/Background";
 import HomeContent from "../components/home/HomeContent";
 
 function Home(props) {
-  const { history } = props;
   return (
     <>
       {/* page는 home 또는 detail. home은 메인페이지, detail은 나머지페이지 */}
       <Header {...props} />
       <Background />
-      <Layout page="home">
+      <Layout>
         <HomeContent {...props} />
       </Layout>
     </>
