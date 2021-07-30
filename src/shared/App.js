b728ea5c7f458ae97c68dd28d7339700b4c7b99f
choice <wer1264@gmail.com>
2021-07-30 12:17:25 +0900

    Rename: components에 option.js들 소문자에서 대문자로 변경

--- src/shared/App.js
+++ src/shared/App.js
@@ -5,9 +5,9 @@ import { Route, BrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 
 import Option01 from "../components/option/Option01";
-import option02 from "../components/option/option02";
-import option03 from "../components/option/option03";
-import option04 from "../components/option/option04";
+import Option02 from "../components/option/Option02";
+import Option03 from "../components/option/Option03";
+import Option04 from "../components/option/Option04";
 import Detail from "../pages/Detail";
 import SignUp from "../pages/SignUp";
 import Login from "../pages/Login";
@@ -19,9 +19,9 @@ function App() {
       <BrowserRouter>
         <Route exact path="/" component={Home} />
         <Route exact path="/option01" component={Option01} />
-        <Route exact path="/option02" component={option02} />
-        <Route exact path="/option03" component={option03} />
-        <Route exact path="/option04" component={option04} />
+        <Route exact path="/option02" component={Option02} />
+        <Route exact path="/option03" component={Option03} />
+        <Route exact path="/option04" component={Option04} />
         <Route exact path="/detail" component={Detail} />
         <Route exact path="/join" component={SignUp} />
         <Route exact path="/login" component={Login} />
